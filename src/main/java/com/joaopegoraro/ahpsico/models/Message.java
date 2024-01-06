e189e033d354c2fd38fe93f57f893526f4c7b30d
João Pegoraro <hahmraro@tuta.io>
2024-01-06 09:46:25 -0300

    added manytomany relationship between User and Message tables

--- src/main/java/com/joaopegoraro/ahpsico/models/Message.java
+++ src/main/java/com/joaopegoraro/ahpsico/models/Message.java
@@ -1,19 +1,25 @@
 package com.joaopegoraro.ahpsico.models;
 
 import java.sql.Date;
+import java.util.Set;
 
 import org.hibernate.annotations.CreationTimestamp;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 
 @Entity
 public class Message {
     @Id
     @GeneratedValue
     private Long id;
+
+    @ManyToMany(mappedBy = "messages", fetch = FetchType.LAZY)
+    private Set<User> users;
 
     @Column(nullable = false)
     private String text;
@@ -28,6 +34,14 @@ public class Message {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
     }
 
     public String getText() {
@@ -45,4 +59,5 @@ public class Message {
     public void setCreatedAt(Date createdAt) {
         this.createdAt = createdAt;
     }
+
 }
--- src/main/java/com/joaopegoraro/ahpsico/models/User.java
+++ src/main/java/com/joaopegoraro/ahpsico/models/User.java
@@ -1,14 +1,19 @@
 package com.joaopegoraro.ahpsico.models;
 
 import java.sql.Date;
+import java.util.Set;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 
 @Entity
 public class User {
@@ -33,6 +38,10 @@ public class User {
 
     @Column(nullable = false)
     private boolean isAdmin;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_messages", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "message_id"))
+    private Set<Message> messages;
 
     @Column(nullable = false)
     @CreationTimestamp
@@ -96,6 +105,14 @@ public class User {
 
     public void setAdmin(boolean isAdmin) {
         this.isAdmin = isAdmin;
+    }
+
+    public Set<Message> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(Set<Message> messages) {
+        this.messages = messages;
     }
 
     public Date getCreatedAt() {
