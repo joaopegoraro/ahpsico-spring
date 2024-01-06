5c45e8f0fa4c7aecce87a111f37a2b43e89c843d
João Pegoraro <hahmraro@tuta.io>
2024-01-06 09:03:00 -0300

    initial commit

---
+++ src/test/java/com/joaopegoraro/ahpsico/AhpsicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaopegoraro.ahpsico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhpsicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
