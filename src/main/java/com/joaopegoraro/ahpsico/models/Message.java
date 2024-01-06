9aa53b24ce392d3d8cfb0cade699a9b7ae74a253
João Pegoraro <hahmraro@tuta.io>
2024-01-06 09:34:46 -0300

    Added Message model

---
+++ src/main/java/com/joaopegoraro/ahpsico/models/Message.java
@@ -0,0 +1,48 @@
+package com.joaopegoraro.ahpsico.models;
+
+import java.sql.Date;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Message {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String text;
+
+    @Column(nullable = false)
+    @CreationTimestamp
+    private Date createdAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+}
