bd515b6a6dc3c0af5653332e011c6ef9a6fc2fe7
João Pegoraro <hahmraro@tuta.io>
2024-01-06 10:57:02 -0300

    added more SessionStatus statuses

--- src/main/java/com/joaopegoraro/ahpsico/enums/SessionStatus.java
+++ src/main/java/com/joaopegoraro/ahpsico/enums/SessionStatus.java
@@ -1,5 +1,5 @@
 package com.joaopegoraro.ahpsico.enums;
 
 public enum SessionStatus {
-    NOT_CONFIRMED, CONFIRMED, CANCELED, CONCLUDED
+    NOT_CONFIRMED, CONFIRMED_BY_PATIENT, CONFIRMED_BY_DOCTOR, CONFIRMED, CANCELED, CONCLUDED
 }
